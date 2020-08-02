3b672fc72d568b5cc9924ec1837c8c34e314d3e5
jutotal <2297312367@qq.com>
2020-08-03 00:50:41 +0800

    第一次提交

---
+++ src/main/java/com/luyi/demo/controller/ApiController.java
@@ -0,0 +1,20 @@
+package com.luyi.demo.controller;
+
+/**
+ * Create by 肖橙橙
+ * Date: 2020:08:03  00:39
+ */
+public class ApiController {
+    /**
+     * 返回方式：
+     * 1.类注解 @RestController
+     * 2.方法注解 @ResponseBody
+     *
+     * 请求方式
+     * 1.@RequestMapping
+     *   在RequestMapping的源码中提到，这种支持任意请求方式
+     * 2.@GetMapping
+     *   客户端只能用 GET 方式请求，适用于查询数据
+     * 3.
+     */
+}
